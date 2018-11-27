86b1e56a9a1bf38c2141d098adea6b9aaf041900
Jaime Araujo <42072923+JaimeAraujo18@users.noreply.github.com>
2018-11-27 18:14:50 -0200

    Ajustes #11

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,9 +5,10 @@ $(function() {
 		var ds2 = $("#diaSeco2");
 		var do1 = $("#diaOrg1");
 		var do2 = $("#diaOrg2");
+		var cid = $("#cidadeID");
 		var event= e;
 
-		if ((ds1.val()=="Selecione um dia") || (ds2.val()=="Selecione um dia") || (do1.val()=="Selecione um dia") || (do2.val()=="Selecione um dia")) 
+		if ((cid.val()=="Selecione uma cidade") || (ds1.val()=="Selecione um dia") || (ds2.val()=="Selecione um dia") || (do1.val()=="Selecione um dia") || (do2.val()=="Selecione um dia")) 
 		{
 			event.preventDefault();
 			$("#erroBairro").removeClass('hidden');
@@ -15,7 +16,7 @@ $(function() {
 	});	
 	$("#formAviso").submit(function(e) {
 		var bid = $("#bairroID");
-		var cid = $("#bairroID");
+		var cid = $("#cidadeID");
 		var event= e;
 
 		if ((bid.val()=="Selecione um bairro") || (cid.val()=="Selecione uma cidade")) 
