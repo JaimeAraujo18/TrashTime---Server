8b47f0233587433506bde7e7438e6d355481751e
Jaime Araujo <42072923+JaimeAraujo18@users.noreply.github.com>
2018-11-27 20:41:02 -0200

    Ajustes #15

--- application/views/pages/bairro.php
+++ application/views/pages/bairro.php
@@ -25,47 +25,53 @@
 						<tbody>
 						<?php
 						if (isset($bairros)) {
+							$dias1;
+							$dias2;
+							$diao1;
+							$diao2;
+							$cidadeNome;
 							foreach ($bairros as $bairro) {
-								$dias1;
-								$dias2;
-								$diao1;
-								$diao2;
-									switch ($bairro['dia_seco1']){
-										case 1:$dias1 = "Domingo";break;
-										case 2:$dias1 = "Segunda-Feira";break;
-										case 3:$dias1 = "Terça-Feira";break;
-										case 4:$dias1 = "Quarta-Feira";break;
-										case 5:$dias1 = "Quinta-Feira";break;
-										case 6:$dias1 = "Sexta-Feira";break;
-										case 7:$dias1 = "Sábado";break;
-									}
-									switch ($bairro['dia_seco2']){
-										case 1:$dias2 = "Domingo";break;
-										case 2:$dias2 = "Segunda-Feira";break;
-										case 3:$dias2 = "Terça-Feira";break;
-										case 4:$dias2 = "Quarta-Feira";break;
-										case 5:$dias2 = "Quinta-Feira";break;
-										case 6:$dias2 = "Sexta-Feira";break;
-										case 7:$dias2 = "Sábado";break;
-									}
-									switch ($bairro['dia_org1']){
-										case 1:$diao1 = "Domingo";break;
-										case 2:$diao1 = "Segunda-Feira";break;
-										case 3:$diao1 = "Terça-Feira";break;
-										case 4:$diao1 = "Quarta-Feira";break;
-										case 5:$diao1 = "Quinta-Feira";break;
-										case 6:$diao1 = "Sexta-Feira";break;
-										case 7:$diao1 = "Sábado";break;
-									}
-									switch ($bairro['dia_org2']){
-										case 1:$diao2 = "Domingo";break;
-										case 2:$diao2 = "Segunda-Feira";break;
-										case 3:$diao2 = "Terça-Feira";break;
-										case 4:$diao2 = "Quarta-Feira";break;
-										case 5:$diao2 = "Quinta-Feira";break;
-										case 6:$diao2 = "Sexta-Feira";break;
-										case 7:$diao2 = "Sábado";break;
-									}
+								foreach ($cidades as $cidade){
+									if($cidade['id']==$bairro['cidade_id']){
+										$cidadeNome = $cidade['nome'];
+									}					
+								}
+								switch ($bairro['dia_seco1']){
+									case 1:$dias1 = "Domingo";break;
+									case 2:$dias1 = "Segunda-Feira";break;
+									case 3:$dias1 = "Terça-Feira";break;
+									case 4:$dias1 = "Quarta-Feira";break;
+									case 5:$dias1 = "Quinta-Feira";break;
+									case 6:$dias1 = "Sexta-Feira";break;
+									case 7:$dias1 = "Sábado";break;
+								}
+								switch ($bairro['dia_seco2']){
+									case 1:$dias2 = "Domingo";break;
+									case 2:$dias2 = "Segunda-Feira";break;
+									case 3:$dias2 = "Terça-Feira";break;
+									case 4:$dias2 = "Quarta-Feira";break;
+									case 5:$dias2 = "Quinta-Feira";break;
+									case 6:$dias2 = "Sexta-Feira";break;
+									case 7:$dias2 = "Sábado";break;
+								}
+								switch ($bairro['dia_org1']){
+									case 1:$diao1 = "Domingo";break;
+									case 2:$diao1 = "Segunda-Feira";break;
+									case 3:$diao1 = "Terça-Feira";break;
+									case 4:$diao1 = "Quarta-Feira";break;
+									case 5:$diao1 = "Quinta-Feira";break;
+									case 6:$diao1 = "Sexta-Feira";break;
+									case 7:$diao1 = "Sábado";break;
+								}
+								switch ($bairro['dia_org2']){
+									case 1:$diao2 = "Domingo";break;
+									case 2:$diao2 = "Segunda-Feira";break;
+									case 3:$diao2 = "Terça-Feira";break;
+									case 4:$diao2 = "Quarta-Feira";break;
+									case 5:$diao2 = "Quinta-Feira";break;
+									case 6:$diao2 = "Sexta-Feira";break;
+									case 7:$diao2 = "Sábado";break;
+								}
 								echo
 							"<tr>
 								<th scope='row'>".$bairro['id']."</th>
@@ -74,7 +80,7 @@
 								<td>".$dias2."</td>
 								<td>".$diao1."</td>
 								<td>".$diao2."</td>
-								<td>".$cidade['nome']."</td>
+								<td>".$cidadeNome."</td>
 								<td class='row' id='td'>
 									<a href='".base_url('/bairro/excluir/'.$bairro['id'])."' class='ml-3 btn btn-danger btn-sm'>Excluir</a>
 									<a href='".base_url('/bairro/editar/'.$bairro['id'])."' class='ml-4 btn btn-primary btn-sm'>Editar</a>
